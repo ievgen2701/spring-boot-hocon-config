ce684c1aa79490bf8e4a041c236bb727ab4a7f52
ievgen2107 <ievgen.anikushyn@gmail.com>
2017-12-27 22:12:59 +0200

    initial commit

---
+++ hocon-demo/src/main/java/faam/boot/hocondemo/HoconDemoApplication.java
@@ -0,0 +1,12 @@
+package faam.boot.hocondemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HoconDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HoconDemoApplication.class, args);
+	}
+}
